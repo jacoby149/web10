53bad103df64c5f419efccbb7176d576596e5200
jacoby149 <jackstudent149@gmail.com>
2022-03-04 13:51:28 -0500

    simplify the folder structure of auth

--- auth/serviceChanger.js
+++
@@ -1,9 +0,0 @@
-
-//Makes the serviceChanger library object
-function serviceChangerInit () {
-    const serviceChanger = {};
-    //output the wapiAuth object
-    return serviceChanger;
-}
-//initialize wapiAuth
-var serviceChanger = serviceChangerInit();
