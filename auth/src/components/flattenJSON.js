606c95a3308565f3bd1bdcbe60b5df0c7d30be78
jacoby149 <jackstudent149@gmail.com>
2022-03-18 13:11:24 -0400

    added length counts to ui

--- auth/src/components/flattenJSON.js
+++ auth/src/components/flattenJSON.js
@@ -1,15 +1,15 @@
 // function for flattening json
 const flattenJSON = (obj = {}, res = {}, extraKey = "") => {
-    for (const key in obj) {
-      if (typeof obj[key] !== "object") {
-        res[extraKey + key] = obj[key];
-      } else if (Object.keys(obj[key]).length === 0) {
-        res[extraKey + key] = obj[key].constructor == Object ? "{}" : "[]";
-      } else {
-        flattenJSON(obj[key], res, `${extraKey}${key}.`);
-      }
+  for (const key in obj) {
+    if (typeof obj[key] !== "object") {
+      res[extraKey + key] = obj[key];
+    } else {
+      res[extraKey + key] =
+        obj[key].constructor == Object ? `{${Object.keys(obj[key]).length}↴` : `[${obj[key].length}↴`;
+      flattenJSON(obj[key], res, `${extraKey}${key}.`);
     }
-    return res;
-  };
+  }
+  return res;
+};
 
 export default flattenJSON;
