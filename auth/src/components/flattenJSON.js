a8860bfd3437605228f4ebea31e212292b624cd1
jacoby149 <jackstudent149@gmail.com>
2022-03-19 14:05:10 -0400

    add JSON unflattening for CRUD

--- auth/src/components/flattenJSON.js
+++ auth/src/components/flattenJSON.js
@@ -12,4 +12,37 @@ const flattenJSON = (obj = {}, res = {}, extraKey = "") => {
   return res;
 };
 
-export default flattenJSON;
+//read an object value given a flat key
+const readFlat = (obj={},flatKey) => {
+  const keyList = flatKey.split(".");
+  const loc = obj;
+  for (const [i, key] of keyList.entries()) {
+    loc = loc[key];
+  }
+  return loc;
+}
+
+//write an object value given a flat key
+const writeFlat = (obj={},flatKey,value) => {
+  const keyList = flatKey.split(".");
+  const loc = obj;
+  for (const [i, key] of keyList.entries()) {
+    //if the last key, set the value
+    if (i===keyList.length-1) loc[key] = value
+    //otherwise keep traversing the object
+    else loc = loc[key];
+  }
+  return loc;
+}
+
+
+// function for unflattening json
+const unFlattenJSON = (flattenedJSON) => {
+  const obj = {};
+  for (const key in flattenedJSON) {
+    writeFlat(obj,key,flattenedJSON[key]);
+  }
+  return obj;
+};
+
+export {flattenJSON,unFlattenJSON};
