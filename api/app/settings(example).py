3bf25ef54560010c9c79dbb61b2f2bb9b8a56ce6
jacoby149 <jackstudent149@gmail.com>
2022-03-20 16:59:05 -0400

    beefed up settings

--- api/app/settings(example).py
+++ api/app/settings(example).py
@@ -1,12 +1,21 @@
+import os
+
 # url of the web10 server online 
-PROVIDER = "api.localhost"
-CORS_SERVICE_MANAGERS = ["auth.localhost"]
+PROVIDER = os.environ.get('PROVIDER')
+if PROVIDER==None : PROVIDER = "api.localhost"
+
+CORS_SERVICE_MANAGERS = os.environ.get('CORS_SERVICE_MANAGERS')
+if CORS_SERVICE_MANAGERS==None : CORS_SERVICE_MANAGERS = ["auth.localhost"]
 
 #https://www.mongodb.com/atlas/database
-DB_URL = "mongodb+srv://web10origin:web10origin@cluster0.lyjne.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+DB_URL = os.environ.get('DB_URL')
+if DB_URL==None : DB_URL = "mongodb+srv://web10:web10@cluster0.jnagr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-# oauth token algorithm
-ALGORITHM = "HS256"
-PRIVATE_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = os.environ.get("ALGORITHM")
+if ALGORITHM==None: ALGORITHM = "HS256"
 
-TOKEN_EXPIRE_MINUTES = 30
+PRIVATE_KEY = os.environ.get('PRIVATE_KEY')
+if PRIVATE_KEY==None : PRIVATE_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+
+TOKEN_EXPIRE_MINUTES = os.environ.get('TOKEN_EXPIRE_MINUTES')
+if TOKEN_EXPIRE_MINUTES==None: TOKEN_EXPIRE_MINUTES = 30
