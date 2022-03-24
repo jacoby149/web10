edc29a5ea0cad9d9e57828072a6788db8f9c7256
jacoby149 <jackstudent149@gmail.com>
2022-03-24 17:28:48 -0400

    import and export helpers

--- auth/src/components/ServiceTerms.js
+++ auth/src/components/ServiceTerms.js
@@ -220,18 +220,6 @@ function clear() {
   return;
 }
 
-function downloadObjectAsJson(exportObj, exportName) {
-  var dataStr =
-    "data:text/json;charset=utf-8," +
-    encodeURIComponent(JSON.stringify(exportObj));
-  var downloadAnchorNode = document.createElement("a");
-  downloadAnchorNode.setAttribute("href", dataStr);
-  downloadAnchorNode.setAttribute("download", exportName + ".json");
-  document.body.appendChild(downloadAnchorNode); // required for firefox
-  downloadAnchorNode.click();
-  downloadAnchorNode.remove();
-}
-
 function EditApproval({ flattenedService, type, SMRIncrement }) {
   return (
     <div>
