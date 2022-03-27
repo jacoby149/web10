1bdb498cd4141672123a99af612cbe17e6fcf443
jacoby149 <jackstudent149@gmail.com>
2022-03-27 19:01:39 -0400

    compartmentalized dbs

--- api/app/mongo.py
+++ api/app/mongo.py
@@ -17,7 +17,7 @@ client_url = os.environ.get("DB_URL")
 if not client_url:
     DB_URL = settings.DB_URL
 client = pymongo.MongoClient(DB_URL)
-db = client["web10"]
+db = client[settings.DB]
 
 
 ################################
