c059360a0a676fc5703c0a8b0cda06acb7c3126b
jacoby149 <jackstudent149@gmail.com>
2022-03-27 08:15:13 -0400

    re implemented charging mechanism

--- api/app/web10records.py
+++ api/app/web10records.py
@@ -5,10 +5,8 @@ def star_record():
         "service": "*",
         "username": "USERNAME",
         "hashed_password": "PASSWORD",
-        "storage_capacity_mb":settings.SPACE,
-        "writes":settings.WRITES,
-        "reads":settings.READS,
-        "deletes":settings.DELETES,
+        "storage_capacity_mb":settings.FREE_SPACE,
+        "credits":settings.FREE_CREDITS,
         "last_refresh":-1,
         # no whitelist or blacklist...
         # this is a web10 exclusive document.
