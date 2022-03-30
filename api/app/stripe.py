e69845e0c6c82d248c34ee2d49a0ac0cdf46a8f8
jacoby149 <jackstudent149@gmail.com>
2022-03-30 14:41:31 -0400

    stripe interface complete

--- api/app/stripe.py
+++ api/app/stripe.py
@@ -3,39 +3,48 @@ import app.settings as settings
 
 stripe.api_key = settings.STRIPE_KEY
 
+################
+# Prices objects
+################
+subscription = [{
+    "price":settings.STRIPE_CREDIT_SUB_ID,"adjustable_quantity":True
+    },
+    {
+    "price":settings.STRIPE_SPACE_SUB_ID,"adjustable_quantity":True
+    }]
+purchase = [{
+    "price":settings.STRIPE_CREDIT_ID,"adjustable_quantity":True
+    }]
+
+
+############################################
+# customer payment / subscription management
+############################################
+def make_customer():
+    customer = stripe.Customer.create(
+        description="New Customer",
+    )
+    return customer["id"]
+
 def create_checkout_session(customer_id,prices,sub=True):
-    #there needs to be a customer id...
-    if not customer_id:
-        customer = stripe.Customer.create(
-            description="New Customer",
-        )
-    
     mode = "payment"
-    if sub : mode = "subscription"
+    prices = purchase
+    if sub : 
+        mode = "subscription"
+        prices = subscription
 
     checkout_session = stripe.checkout.Session.create(
-        customer=customer,
-        #success_url="https://localhost:8000/success?session_id={CHECKOUT_SESSION_ID}",
-        #cancel_url="http://localhost:8000/cancel",
+        customer=customer_id,
         payment_method_types=["card"],
         mode=mode,
         line_items= prices
-        # [{
-        #     "price": data["priceId"],
-        #     "quantity": 4
-        # },{
-        #     "price": "price_1Ki4vvFGm9S2yiuxLklHRK3a",
-        #     "quantity": 1
-        # }],
     )
     return {
-        "sessionUrl": checkout_session["url"],"customerId":customer_id
+        "sessionUrl":checkout_session["url"],"customerId":customer_id
         }
-
 
 def create_portal_session(customer_id):
     session = stripe.billing_portal.Session.create(
         customer=customer_id,
-        # return_url="http://localhost:8000"
     )
     return {"url": session.url}
