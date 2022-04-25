c027bf3b7dd0be4c15094e2a2632d52e04e60256
jacoby149 <jackstudent149@gmail.com>
2022-04-25 18:43:01 -0400

    Revert "patch"
    This reverts commit d299bcd8daafef59fcffa453dfee0e71c4279674.

--- auth/public/sdk/wapi.js
+++ auth/public/sdk/wapi.js
@@ -13,12 +13,11 @@ if (typeof wapiInit === "undefined") {
   }
 
   //initializes the wapi library object
-  function wapiInit(authUrl = "https://auth.web10.app", rtcUrl = "https://rtc.web10.app",protocol=null) {
+  function wapiInit(authUrl = "https://auth.web10.app", rtcUrl = "https://rtc.web10.app") {
     const wapi = {};
 
     // get the default api protocol, which is required to match its auth portals protocol
-    if (protocol) wapi.defaultAPIProtocol = protocol + ":";
-    else wapi.defaultAPIProtocol = new URL(authUrl).protocol;
+    wapi.defaultAPIProtocol = new URL(authUrl).protocol;
 
     //wapi variables
     wapi.childWindow = null;
