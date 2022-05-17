81df06ea4ca54304c6dddac66327cafd6d3d9297
jacoby149 <jackstudent149@gmail.com>
2022-05-16 23:19:52 -0400

    good initial start to the crypto journey

--- mobile/encryptor/manage.js
+++
@@ -1,8 +0,0 @@
-// handles authentication and changing passwords
-
-//sends a verification code to a phone number IF it is associated with a web10 acct.
-const sendcode = function(phoneNumber){
-
-}
-
-const logIn(code,phoneNumber):
