55460b8ccff85f9c4d9fd0d3b069d2f4f00e38f2
jacoby149 <jackstudent149@gmail.com>
2022-07-22 15:09:06 -0400

    getting more generous with .env files

--- api/app/settings.py
+++ api/app/settings.py
@@ -1,5 +1,9 @@
 import os
 import json
+from dotenv import load_dotenv
+
+# in case vars are in a dotenv file
+load_dotenv()
 
 # goes through variables in the config file 
 # checks if they exist and env vars
@@ -7,7 +11,7 @@ import json
 config_file = open("config.json")
 config = json.loads("config.json")
 for key in config :
-    val = os.environ.get("PROVIDER")
+    val = os.getenv("PROVIDER")
     if val == None:
         val = config[key]
     globals()[key] = val
