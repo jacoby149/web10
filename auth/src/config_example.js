7f15e5922a1f3b3b2f5d017af18094e98f25420b
jacoby149 <jackstudent149@gmail.com>
2022-07-24 17:04:58 -0400

    make it all false for simplicity

--- auth/src/config_example.js
+++ auth/src/config_example.js
@@ -1,6 +1,6 @@
 var config = {
     DEFAULT_API: process.env.REACT_APP_DEFAULT_API || "api.localhost",
-    BETA_REQUIRED: process.env.REACT_APP_BETA_REQUIRED || true,
+    BETA_REQUIRED: process.env.REACT_APP_BETA_REQUIRED || false,
     VERIFY_REQUIRED: process.env.REACT_APP_VERIFY_REQUIRED || false,
     PAY_REQUIRED: process.env.REACT_APP_PAY_REQUIRED || false,
 }
