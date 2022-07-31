b1836e23bf449571286ea4d4ee5f8c22e53d9aeb
jacoby149 <jackstudent149@gmail.com>
2022-07-31 11:01:31 -0400

    added PWA to wapi auth

---
+++ auth/public/serviceWorker.js
@@ -0,0 +1,18 @@
+const staticApp = "rectangles plainpad"
+const assets = []
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticApp).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
