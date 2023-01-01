60b7cbc3d7965b02ee2b0244d5f2f1d691d167fe
jacoby149 <jackstudent149@gmail.com>
2023-01-01 17:03:57 -0500

    Not so bad

--- auth2/src/components/Settings/SettingsPage.js
+++ auth2/src/components/Settings/SettingsPage.js
@@ -6,25 +6,18 @@ import { R } from 'rectangles-npm'
 import TopBar from '../shared/TopBar';
 import SideBar from '../shared/SideBar';
 
-function Contracts({ I }) {
+function SettingsPage({ I }) {
     return (
         <R root t bt bb br bl theme={I.theme}>
             <TopBar I={I}></TopBar>
             <R l tel>
                 <SideBar I={I}></SideBar>
                 <R t tel>
-                <div style={{backgroundColor:"#FFE799"}} className="box contract">
-                        Contacts
-                    </div><div style={{backgroundColor:"#D7FFEE"}} className="box contract">
-                        Posts
-                    </div>
-                    <div style={{backgroundColor:"white"}} className="box contract">
-                        Feed
-                    </div>
+                    <Settings I={I}/>
                 </R>
             </R>
         </R>
     );
 }
 
-export default Contracts;
+export default SettingsPage;
