17f27daa4fa7251484df09110d95ad489e0f3b89
jacoby149 <jackstudent149@gmail.com>
2023-01-04 17:31:01 -0500

    mode switching

--- auth2/src/components/Contracts/Contract.js
+++ auth2/src/components/Contracts/Contract.js
@@ -1,11 +1,15 @@
 import ContractViewer from "./components/ContractViewer";
+import ContractEditor from "./components/ContractEditor";
 import useContractInterface from "../../interfaces/ContractInterface";
 
 function Contract({ I, data, isRequest}){
-    const contractI=useContractInterface(I,data,isRequest)
+    const contractI = useContractInterface(I,data,isRequest)
     // I , contractI
-    return <ContractViewer I={I} contractI={contractI} />
-    //ContractEditor Save Changes, Delete Changes
+    if (contractI.mode==="view")
+        return <ContractViewer I={I} contractI={contractI} />
+    else
+        return <ContractEditor I={I} contractI={contractI}></ContractEditor>
+        //ContractEditor Save Changes, Delete Changes
     //RequestViewer Approve Edit Deny
     // RequestEditor Save Changes ||  Cancel Changes
 }
--- auth2/src/interfaces/ContractInterface.js
+++ auth2/src/interfaces/ContractInterface.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 function useContractInterface(I, data = null,isRequest) {
-    const contractI = {}
-    contractI.data = data
+    const contractI = {};
+    
+    [contractI.data, contractI.setData] = React.useState(data);
+    [contractI.mode,contractI.setMode] = React.useState("view");
 
+    contractI.edit = function(){
+        contractI.setMode("edit");
+    }
+    contractI.view = function(){
+        contractI.setMode("view");
+    }
     return contractI;
 }
 
