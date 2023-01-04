ad4980445823b5767eb660875c3bd160debeb193
jacoby149 <jackstudent149@gmail.com>
2023-01-04 15:52:34 -0500

    battle plan for all kinds of contract shit

---
+++ auth2/src/components/Contracts/RequestPage.js
@@ -0,0 +1,37 @@
+// see stuff about a friend + friend group
+// pictures, media , text about them. 
+// also configure ( manage notifications / unfriend / unfollow )
+
+import { R } from 'rectangles-npm'
+import TopBar from '../shared/TopBar';
+import SideBar from '../shared/SideBar';
+import Contract from './Contract';
+
+function Requests({ I }) {
+    console.log(I.services)
+    const contract_items = I.services.map((d,i)=>
+        <Contract I={I} key={i} data ={d} />
+    )
+    return (
+        <>
+            <div style={{ margin: "15px 0px 0px 0px" }} className="center-container"><b>Contracts - jacoby149</b></div>
+            {contract_items}
+        </>
+    )
+}
+
+function RequestPage({ I }) {
+    return (
+        <R root t bt bb br bl theme={I.theme}>
+            <TopBar I={I}></TopBar>
+            <R l tel>
+                <SideBar I={I}></SideBar>
+                <R t tel>
+                    <Contracts I={I} />
+                </R>
+            </R>
+        </R>
+    );
+}
+
+export default ContractPage;
