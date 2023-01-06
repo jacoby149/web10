a03cdb55846def867ca19d355cbea0bfe3d87077
jacoby149 <jackstudent149@gmail.com>
2023-01-06 15:54:24 -0500

    editing en route

--- auth2/src/components/Contracts/components/ContractComponents.js
+++ auth2/src/components/Contracts/components/ContractComponents.js
@@ -1,6 +1,6 @@
 {/* <div key={i} style={{marginLeft:"15px"}}><a>- {site}</a></div> */ }
 
-function Tag({ text,color }) {
+function Tag({ text, color }) {
     return <span style={{ margin: "0px 4px 4px 0px" }} className={`tag is-${color} is-light is-normal`}>{text} {/*<button className="delete is-small"></button>*/}</span>
 }
 
@@ -13,24 +13,27 @@ function Websites({ contractI }) {
 
 function PermissionsList({ permissions, title }) {
     const permission_items = permissions.map((p, i) => {
-        const create = p.create ? <Tag text="create" color="primary"/> : ""
-        const read = p.read ? <Tag text="read" color="info"/> : ""
-        const update = p.update ? <Tag text="create" color="warning"/> : ""
-        const del = p.delete ? <Tag text="create" color="danger"/> : ""
+        const create = p.create ? <Tag text="create" color="primary" /> : ""
+        const read = p.read ? <Tag text="read" color="info" /> : ""
+        const update = p.update ? <Tag text="create" color="warning" /> : ""
+        const del = p.delete ? <Tag text="create" color="danger" /> : ""
 
         return (
+
             <div key={i}>
-                <a style={{ marginLeft: "15px"}}> {p.provider}/{p.username} : </a>
-                    {create}
-                    {read}
-                    {update}
-                    {del}
+                <a style={{ marginLeft: "15px" }}> {p.provider}/{p.username} : </a>
+                {create}
+                {read}
+                {update}
+                {del}
+                {/* <i style={{ color: "red", marginLeft: "5px" }} className={"fa fa-trash font-weight-bold"}></i> */}
             </div>
+
         )
     })
     return (
         permissions.length > 0 ?
-            <div style={{marginTop:"10px"}}> <div style={{marginBottom:"4px"}}><u>{title}</u> :</div> {permission_items}</div> :
+            <div style={{ marginTop: "10px" }}> <div style={{ marginBottom: "4px" }}><u>{title}</u> :</div> {permission_items}</div> :
             <></>
     )
 }
@@ -43,4 +46,4 @@ function WhiteList({ contractI }) {
     return <PermissionsList permissions={contractI.data.blacklist} title={"Allowed users"} />
 }
 
-export {Websites,BlackList,WhiteList}
+export { Websites, BlackList, WhiteList }
