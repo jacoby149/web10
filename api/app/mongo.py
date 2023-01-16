b4d99186072894daafa9510d8a82079a961c939f
jacoby149 <jackstudent149@gmail.com>
2023-01-16 16:50:31 -0500

    nice little todo

--- api/app/mongo.py
+++ api/app/mongo.py
@@ -250,7 +250,8 @@ def delete(user, service, query):
         raise exceptions.STAR
     query = q_t(query, service)
     db[f"{user}"].delete_many(query)
-    return "success"
+    # TODO return {deletedCount : response.deleted_count} if possible
+    return "successfully deleted"
 
 
 
