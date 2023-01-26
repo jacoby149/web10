3010fad35ae55c804696f885a0ae13d16c8d1c41
jacoby149 <jackstudent149@gmail.com>
2023-01-26 00:59:55 -0500

    demo-ready

--- auth2/src/mocks/mockAppData.js
+++ auth2/src/mocks/mockAppData.js
@@ -1,71 +1,46 @@
-import img1 from "../assets/images/stealth.jpg"
+import authImg from "../assets/images/key_white.png"
+import socialImg from "../assets/images/alternative.png"
+import stealthImg from "../assets/images/stealth.jpg"
+import fpsImg from "../assets/images/shooter.jpeg"
+import crmImg from "../assets/images/GreenStarWhite.png"
 
 
 var mockPage = [
     {
-        title: "title1",
+        title: "web10 auth",
         description: "description1",
-        img : img1,
-        hits:3245,
+        href:"http://auth.localhost",
+        img : authImg,
+        hits:1194,
     },
     {
-        title: "title2",
+        title: "web10 social",
         description: "description2",
-        hits:3156,
+        href:"http://localhost:3001",
+        img : socialImg,
+        hits:785,
     },
     {
-        title: "title3",
+        title: "crm",
         description: "description3",
-        img : img1,
-        hits:3072,
+        href:"https://crm.web10.app",
+        img : crmImg,
+        hits:535,
     },
     {
-        title: "title4",
-        description: "description4",
-        hits:2865,
-
+        title: "ball shooter",
+        description: "description3",
+        href:"https://jacobhoffman.tk/fps",
+        img : fpsImg,
+        hits:228,
     },
     {
-        title: "title5",
-        description: "description5",
-        img : img1,
-        hits:2346,
+        title: "stealth",
+        description: "description3",
+        href:"",
+        img : stealthImg,
+        hits:37,
     },
-    {
-        title: "title6",
-        description: "description6",
-        hits:2243,
-    },
-    {
-        title: "title7",
-        description: "description7",
-        hits:1999,
-    },
-    {
-        title: "title8",
-        description: "description8",
-        hits:1876,
-    },
-    {
-        title: "title9",
-        description: "description9",
-        hits:1651,
-    },
-    {
-        title: "title10",
-        description: "description10",
-        hits:1451,
-    },
-    {
-        title: "title11",
-        description: "description11",
-        hits:1232,
-    },
-    {
-        title: "title12",
-        description: "description12",
-        hits:762,
-    }
 ]
 
 export default mockPage;
