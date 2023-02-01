c262794b7b05dd004ab33f0597bab6f494f71a44
jacoby149 <jackstudent149@gmail.com>
2023-02-01 01:30:27 -0500

    lets give this a shot!

--- auth2/src/interfaces/appListingInterface.js
+++ auth2/src/interfaces/appListingInterface.js
@@ -2,18 +2,30 @@ import axios from "axios";
 import React from "react";
 
 // pulls the app information from the publisher.
-function useAppListingInterface(initApp) {
+function useAppListingInterface(initApp, I) {
     const appLI = {};
-    [appLI.app,appLI.setApp] = React.useState(initApp);
+    [appLI.app, appLI.setApp] = React.useState(initApp);
     React.useEffect(() => {
-        return
-        // fetch(`${initApp.href}manifest.json`)
-        //     .then((r) => {
-        //         console.log(r)
-        //     });
+        if (!I.isMock) {
+            const provider = window.location.protocol === "https:" ?
+                "https://api.web10.app" :
+                "http://api.localhost"
+            axios.get(`${provider}/pwa_listing?url=${initApp.href}`)
+                .then((r) => {
+                    console.log(r.data);
+                    const icons = r.data.icons
+                    const icon = icons[icons.length - 1].src
+                    const imgURL = `${initApp.href}${icon}`
+                    appLI.setApp({
+                        ...appLI.app,
+                        title: r.data.name,
+                        img: imgURL
+                    })
+                });
+        }
     }, [initApp])
 
-    appLI.getApp = function(){
+    appLI.getApp = function () {
         return appLI.app;
     }
 
