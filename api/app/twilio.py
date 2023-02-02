c94fec60a9bf778157936fe94398cab00e63c0f0
jacoby149 <jackstudent149@gmail.com>
2023-02-02 15:08:02 -0500

    starting twilio + school vision

--- api/app/twilio.py
+++ api/app/twilio.py
@@ -3,6 +3,7 @@ import os
 from twilio.rest import Client
 import app.settings as settings
 import app.exceptions as exceptions
+from twilio.twiml.messaging_response import MessagingResponse
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
@@ -35,3 +36,32 @@ def check_verification(phone_number,code):
     if verification_check.status != "approved":
         raise exceptions.WRONG_CODE
     return verification_check.sid
+
+
+
+########## Forgot Password #########
+
+# tells the customer how to reset their pass.
+def recovery_prompt():
+    message = client.messages.create(
+                                body='This is web10 account recovery. Your username is "jacoby149". to reset your password, text "RESET" . Otherwise, have a nice day :) ',
+                                from_='+15017122661',
+                                to='+15558675310'
+                            )
+    return message.sid
+
+#https://www.twilio.com/docs/messaging/guides/webhook-request
+# https://www.twilio.com/blog/build-secure-twilio-webhook-python-fastapi
+# sends the reset password to the customer. on them typing RESET
+
+def recovery_response(text):
+    # Start our TwiML response
+    resp = MessagingResponse()
+    resp.message("Your password has been reset to qopwenzxzm")
+    return str(resp)
+
+# sends a prompt if a person texts anything that is not RESET
+def actionless_response():
+    resp = MessagingResponse()
+    resp.message('This is web10 account recovery. Type "RESET" to reset your password. otherwise, your text will be ignored.')
+    return str(resp)
