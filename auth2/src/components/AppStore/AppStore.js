a17e86df9f5a3115bd08d1d25699f98bbb6563c2
Gabriel Lulu <glulu4444@gmail.com>
2023-05-23 13:19:44 -0400

    Added Button
    Added dropdown menu to AppStore.js

--- auth2/src/components/AppStore/AppStore.js
+++ auth2/src/components/AppStore/AppStore.js
@@ -5,7 +5,9 @@
 import { R } from 'rectangles-npm'
 import TopBar from '../shared/TopBar';
 import SideBar from '../shared/SideBar';
-import { Apps } from './Apps'
+import { Apps } from './Apps';
+
+import { Box, Dropdown, Icon, DropdownItem } from 'react-bulma-components';
 
 function AppStore({ I }) {
     return (
@@ -31,9 +33,42 @@ function AppStore({ I }) {
                             <div></div>
                         </div>
                     </div>
+                    <div style={{
+                        display: 'flex',
+                        // bottom: 0,
+                        // right: 0,
+                        // margin: 10,
+                        justifyContent: 'center',
+                        // // height: 600,
+                        // minHeight: '100vh'
+                    }}>
+                        <Dropdown
+                            closeOnSelect={true}
+                            icon={<Icon><i aria-hidden="true" className="fas fa-angle-up" /></Icon>}
+                            label="Add an App!"
+                            color='info'
+                            up='true'
+                        // hoverable='true'
+                        >
+
+                            <Dropdown.Item renderAs="a" value="item">
+                                Option 1
+                            </Dropdown.Item>
+                            <Dropdown.Item renderAs="a" value="other">
+                                Option 2
+                            </Dropdown.Item>
+                            <Dropdown.Item renderAs="b" value="other">
+                                Option 3
+                            </Dropdown.Item>
+                            <Dropdown.Item renderAs="c" value="other">
+                                Option 3
+                            </Dropdown.Item>
+                        </Dropdown>
+                    </div>
                 </R>
             </R>
         </R>
+         
     );
 }
 
