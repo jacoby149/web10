9fa5c2777b3a20b6ced9a26f2752aed3f104facb
jacoby149 <jackstudent149@gmail.com>
2023-08-05 19:02:17 -0400

    cli looking and feeling great

--- web10-cli/bin/create.js
+++ web10-cli/bin/create.js
@@ -1,28 +1,55 @@
-const readline = require('readline');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
 
-function create() {
-    const readlineInterface = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-    function promptUser() {
-        readlineInterface.question('How can I assist you? ', (answer) => {
-            // Process the user's input
-            if (answer.toLowerCase().includes('hello')) {
-                console.log('Hello there!');
-            } else if (answer.toLowerCase().includes('goodbye')) {
-                console.log('Goodbye, have a nice day!');
-                readlineInterface.close();
-                return;
-            } else {
-                console.log('I\'m sorry, I don\'t understand. Can you please rephrase?');
-            }
+async function create() {
 
-            // Continue the conversation recursively
-            promptUser();
-        });
-    }
-    promptUser();
+    /* Choice of template */
+    const templateOptions = ['Notes App', 'Mail App'];
+    console.log("Which template would you like?")
+    const templateAnswer = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'selectedOption',
+            message: 'Select an option:',
+            choices: templateOptions,
+        },
+    ]);
+    console.log(`You selected: ${templateAnswer.selectedOption}`);
+    console.log('')
+
+    /* Choice of framework */
+    const frameworkOptions = ['Vanilla JS + HTML + CSS', 'React'];
+    console.log("Which framework would you like?")
+    const frameworkAnswer = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'selectedOption',
+            message: 'Select an option:',
+            choices: frameworkOptions,
+        },
+    ]);
+    console.log(`You selected: ${frameworkAnswer.selectedOption}`);
+    console.log('')
+
+    /* Choice of Folder Name */
+    console.log("Folder Name")
+    const folderAnswser = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What will the folder be named?',
+        },
+    ]);
+    console.log(`Your folder will be named ${folderAnswser.name}`)
+    console.log('')
+
+    /* Final messages */
+    const greenCheck = chalk.green('\u2713');
+    console.log(`${greenCheck} success.`)
+    console.log(`Creating Folder : (${folderAnswser.name})`);
+    console.log(`Creating your app : (${templateAnswer.selectedOption})`)
+    console.log(`App Created in framework : (${frameworkAnswer.selectedOption})`)
+    console.log('')
 }
 
 exports.create = create;
