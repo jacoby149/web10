206ac7e6b705cd86f4dce033fbd467bf4a64ba8e
Jacob Hoffman <jackstudent149@gmail.com>
2023-10-21 00:22:52 -0400

    added node

---
+++ api/app/bitwarden.py
@@ -0,0 +1,18 @@
+"""
+bitwarden.py
+An interface to bitwarden. 
+gets secrets from bitwarden.
+web10 uses bitwarden as a secrets manager.
+"""
+import settings
+
+def get_bitwarden_secrets():
+    """
+        get the secrets from bitwarden.
+        uses an object_id + private key from settings.
+    """
+    return {"nah":"nah"}
+
+# run bitwarden as a test.
+if __name__=="__main__":
+    print(get_bitwarden_secrets())
